7743c820d925c38e4af07fcb90e83e571ae0827d
fgonzalez <fgonzalez@makingsense.com>
2024-06-12 08:56:38 -0300

    fix: modify UI on review

--- admin/partials/forms-create.php
+++ admin/partials/forms-create.php
@@ -228,9 +228,11 @@
         <div class="panel-header">
           <h2><?php _e('Consent Field settings', 'doppler-form')?></h2>
         </div>
-        <p class="hlp"><?php _e('What is it? Press','doppler-form')?> <?= '<a href="'.__('https://help.fromdoppler.com/en/general-data-protection-regulation?utm_source=landing&utm_medium=integracion&utm_campaign=wordpress', 'doppler-form').'" target="blank">'.__('HELP','doppler-form').'</a>'?>.</p>
-        <button type="button" id="gdpr_add_button" class="dp-button primary-green button-small mt-1"><?php _e('Add new consent','doppler-form') ?></button>
-        <ul class="accordion panel-body grid" id="gdpr_section"></ul>
+        <div class="panel-body grid">
+          <span class="hlp"><?php _e('What is it? Press','doppler-form')?> <?= '<a href="'.__('https://help.fromdoppler.com/en/general-data-protection-regulation?utm_source=landing&utm_medium=integracion&utm_campaign=wordpress', 'doppler-form').'" target="blank">'.__('HELP','doppler-form').'</a>'?>.</span>
+          <button type="button" id="gdpr_add_button" class="dp-button primary-green button-small button-right"><?php _e('Add new consent','doppler-form') ?></button>
+          <ul class="accordion panel-body grid mt-1" id="gdpr_section"></ul>
+        </div>
       </div>
     </div>
     
