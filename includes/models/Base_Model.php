bc0b38c31d7b3fa4a60b596c6628e62a1bdb948c
fgonzalez <fgonzalez@makingsense.com>
2024-12-22 20:46:13 -0300

    chore: move events table init to base model

--- includes/models/Base_Model.php
+++ includes/models/Base_Model.php
@@ -161,5 +161,26 @@ class DPLR_Base_Model {
     }
   }
 
+  protected static function initEventsTable() {
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $tablemame = self::_eventTable();
+
+    if($wpdb->get_var("SHOW TABLES LIKE '$tablemame'") != $tablemame) {
+      $sql = "CREATE TABLE ". $eventablemametTableName . "("
+  		 . "id mediumint(9) NOT NULL AUTO_INCREMENT,"
+      . "parent_id mediumint(9) NOT NULL,"
+      . "event_type TINYINT NOT NULL,"
+      . "event_date DATETIME NOT NULL,"
+      . "data TEXT NULL,"
+      . "PRIMARY KEY (id),"
+      . "FOREIGN KEY (parent_id) REFERENCES " . self::_table() . "(".self::$primary_key.") ON DELETE CASCADE"
+      . ") $charset_collate;";
+
+      require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+      dbDelta( $sql );
+    }
+  }
 }
 ?>
