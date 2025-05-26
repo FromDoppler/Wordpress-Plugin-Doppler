efdebd8db47ea02e2a35e2a3b1629782a7568dd7
fgonzalez <fgonzalez@makingsense.com>
2025-05-26 08:23:15 -0300

    chore: remove old files

--- admin/partials/tabs-nav.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-?>
-<h2 class="nav-tab-wrapper">
-    <a href="?page=doppler_forms_main&tab=forms" class="nav-tab <?php echo $active_tab == 'forms' ? 'nav-tab-active' : ''; ?>"><?php _e('Forms', 'doppler-form')?></a>
-</h2>
